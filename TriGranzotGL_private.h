fb17ee13138db42b0ed58244ac9e06bf9dfc711f
sigmak <dwfree74@gmail.com>
2015-11-29 22:33:46 +0900

    ver 0.0.0.1

---
+++ TriGranzotGL_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef TRIGRANZOTGL_PRIVATE_H
+#define TRIGRANZOTGL_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.1"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.0.0.1"
+#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"TriGranzotGL"
+#define PRODUCT_VERSION	"0.0.0.1"
+
+#endif /*TRIGRANZOTGL_PRIVATE_H*/
