65bdd47996199f2380f7eb680cdf6dd9b7adcbb1
sigmak <dwfree74@gmail.com>
2015-12-02 01:46:01 +0900

    v0.0.0.1

---
+++ SubCalc_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef SUBCALC_PRIVATE_H
+#define SUBCALC_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.0.1"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	1
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.0.0.1"
+#define FILE_DESCRIPTION	"Developed using the Dev-C++ IDE"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	""
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	"SubCalc"
+#define PRODUCT_VERSION	"0.0.0.1"
+
+#endif /*SUBCALC_PRIVATE_H*/
